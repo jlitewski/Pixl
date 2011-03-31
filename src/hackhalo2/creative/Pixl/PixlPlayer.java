f9148ca3460665758cf9b41fceb966f1814a0d4e
HACKhalo2 <hackhalotwo@gmail.com>
2011-03-31 01:53:39 -0400

    General code cleanup, added LGPL.txt license

--- src/hackhalo2/creative/Pixl/PixlPlayer.java
+++ src/hackhalo2/creative/Pixl/PixlPlayer.java
@@ -10,81 +10,82 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 
 public class PixlPlayer extends PlayerListener {
-	private final Object _lock = new Object();
-	private final Pixl plugin;
-	
-	public PixlPlayer(Pixl p) { this.plugin = p; }
-	
-	public class detect {
-		private final int[] supported = { 17, 18, 19, 35, 43, 44, 53, 67 }; //Supported Block ID's
-		public detect() { }
-		
-		public boolean Block(Block b) {
-		    for (int i : supported) {
-			if(ID(b) == supported[i]) { return true; }
-		    }
-		    return false;
-		}
-		
-		public Material Type(Block b) { return b.getType(); }
-		
-		public int ID(Block b) { return b.getTypeId(); }
+    public class helper {
+	private final int[] supported = { 17, 18, 19, 35, 43, 44, 53, 67 }; //Supported Block ID's
+	public helper() { }
+
+	public boolean Block(Block b) {
+	    for (int i : supported) {
+		if(ID(b) == supported[i]) { return true; }
+	    }
+	    return false;
 	}
-	
-	@Override
-	public void onPlayerInteract(PlayerInteractEvent e) {
-	    detect a = new detect();
-	    if(e.getAction().toString().equals("RIGHT_CLICK_BLOCK") && a.Block(e.getClickedBlock())) {
-		synchronized(_lock) {
-		    if(plugin.checkPermissions(e.getPlayer(), "pixl.use", false) && e.getPlayer().getItemInHand().getType() == Material.AIR)
-		    	{ pixlArt(e.getClickedBlock(), e.getPlayer()); }
-		}
+
+	public Material Type(Block b) { return b.getType(); }
+
+	public int ID(Block b) { return b.getTypeId(); }
+    }
+    
+    private final Object _lock = new Object();
+    private final Pixl plugin;
+    private final helper a = new helper();
+
+    public PixlPlayer(Pixl p) { this.plugin = p; }
+
+
+
+    @Override
+    public void onPlayerInteract(PlayerInteractEvent e) {
+	if(e.getAction().toString().equals("RIGHT_CLICK_BLOCK") && a.Block(e.getClickedBlock())) {
+	    synchronized(_lock) {
+		if(plugin.checkPermissions(e.getPlayer(), "pixl.use", false) && e.getPlayer().getItemInHand().getType() == Material.AIR)
+		{ pixlArt(e.getClickedBlock(), e.getPlayer()); }
 	    }
 	}
-	
-	@Override
-	public void onPlayerJoin(PlayerJoinEvent e) {
-		//TODO load user settings upon login
-		//TODO message user that Pixl is enabled/disabled on login
-		if(plugin.isToggled(e.getPlayer())) 
-			{ plugin.setToggle(e.getPlayer(), false); }
-	}
-	
-	public void pixlArt(Block b, Player p) {
-		detect a = new detect();
-		//Very hackish detection
-		BlockBreakEvent event1 = new BlockBreakEvent(b, p);
-		plugin.getServer().getPluginManager().callEvent(event1);
-		if(event1.isCancelled()) {
-			return;
-		} else {
-			Block previousBlock = b;
-			if (a.Type(b) == Material.LOG || a.Type(b) == Material.LEAVES) {
-				if(b.getData() < (byte)(2)) { b.setData((byte)(b.getData()+1)); } //add one
-				else { b.setData((byte)(0)); } //reset it
-			} else if (a.Type(b) == Material.WOOL) {
-				if(plugin.isSet(p) == null) {
-					if(b.getData() < (byte)(15)) { b.setData((byte)(b.getData()+1)); } //add one
-					else { b.setData((byte)(0)); } //reset it
-				} else { b.setData(plugin.isSet(p).byteValue()); } //should set the byte value...
-			} else if (a.Type(b) == Material.STEP || a.Type(b) == Material.DOUBLE_STEP) {
-				if(b.getData() < (byte)(3)) { b.setData((byte)(b.getData()+1)); } //add one
-				else { b.setData((byte)(0)); } //reset it
-			} else if (a.Type(b) == Material.WOOD_STAIRS || a.Type(b) == Material.COBBLESTONE_STAIRS) {
-				if(b.getData() == (byte)(0)) { //Ascending south
-					b.setData((byte)(2)); //Ascending west
-				} else if(b.getData() == (byte)(1)) { //Ascending north
-					b.setData((byte)(3)); //Ascending east
-				} else if(b.getData() == (byte)(2)) { //Ascending west
-					b.setData((byte)(1)); //Ascending north
-				} else if(b.getData() == (byte)(3)) { //Ascending east
-					b.setData((byte)(0)); //Ascending south
-				}
-			} else if (a.Type(b) == Material.SPONGE) { //Turn Sponges into fences!
-				b.setType(Material.FENCE);
-			}
-			BlockPlaceEvent event2 = new BlockPlaceEvent(b, previousBlock.getState(), previousBlock, p.getItemInHand(), p, true);
-			plugin.getServer().getPluginManager().callEvent(event2);
+    }
+
+    @Override
+    public void onPlayerJoin(PlayerJoinEvent e) {
+	//TODO load user settings upon login
+	//TODO message user that Pixl is enabled/disabled on login
+	if(plugin.isToggled(e.getPlayer())) 
+	{ plugin.setToggle(e.getPlayer(), false); }
+    }
+
+    public void pixlArt(Block b, Player p) {
+	//Very hackish detection
+	BlockBreakEvent event1 = new BlockBreakEvent(b, p);
+	plugin.getServer().getPluginManager().callEvent(event1);
+	if(event1.isCancelled()) {
+	    return;
+	} else {
+	    Block previousBlock = b;
+	    if (a.Type(b) == Material.LOG || a.Type(b) == Material.LEAVES) {
+		if(b.getData() < (byte)(2)) { b.setData((byte)(b.getData()+1)); } //add one
+		else { b.setData((byte)(0)); } //reset it
+	    } else if (a.Type(b) == Material.WOOL) {
+		if(plugin.isSet(p) == null) {
+		    if(b.getData() < (byte)(15)) { b.setData((byte)(b.getData()+1)); } //add one
+		    else { b.setData((byte)(0)); } //reset it
+		} else { b.setData(plugin.isSet(p).byteValue()); } //should set the byte value...
+	    } else if (a.Type(b) == Material.STEP || a.Type(b) == Material.DOUBLE_STEP) {
+		if(b.getData() < (byte)(3)) { b.setData((byte)(b.getData()+1)); } //add one
+		else { b.setData((byte)(0)); } //reset it
+	    } else if (a.Type(b) == Material.WOOD_STAIRS || a.Type(b) == Material.COBBLESTONE_STAIRS) {
+		if(b.getData() == (byte)(0)) { //Ascending south
+		    b.setData((byte)(2)); //Ascending west
+		} else if(b.getData() == (byte)(1)) { //Ascending north
+		    b.setData((byte)(3)); //Ascending east
+		} else if(b.getData() == (byte)(2)) { //Ascending west
+		    b.setData((byte)(1)); //Ascending north
+		} else if(b.getData() == (byte)(3)) { //Ascending east
+		    b.setData((byte)(0)); //Ascending south
 		}
+	    } else if (a.Type(b) == Material.SPONGE) { //Turn Sponges into fences!
+		b.setType(Material.FENCE);
+	    }
+	    BlockPlaceEvent event2 = new BlockPlaceEvent(b, previousBlock.getState(), previousBlock, p.getItemInHand(), p, true);
+	    plugin.getServer().getPluginManager().callEvent(event2);
 	}
+    }
 }
--- src/hackhalo2/creative/Pixl/PixlServer.java
+++ src/hackhalo2/creative/Pixl/PixlServer.java
@@ -1,7 +1,5 @@
 package hackhalo2.creative.Pixl;
 
-import java.util.logging.Level;
-
 import me.taylorkelly.help.Help;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
@@ -11,72 +9,72 @@ import org.bukkit.event.server.ServerListener;
 import org.bukkit.plugin.Plugin;
 
 public class PixlServer extends ServerListener {
-	
-	private Pixl plugin;
-	private boolean helpFail = false;
-	private boolean permissionsFail = false;
-	
-	public PixlServer(final Pixl p) {
-		this.plugin = p;
+
+    private Pixl plugin;
+    private boolean helpFail = false;
+    private boolean permissionsFail = false;
+
+    public PixlServer(final Pixl p) {
+	this.plugin = p;
+    }
+
+    @Override
+    public void onPluginEnable(PluginEnableEvent e) {
+	Plugin test = plugin.getServer().getPluginManager().getPlugin("Permissions");
+	Plugin test2 = plugin.getServer().getPluginManager().getPlugin("GroupManager");
+	Plugin test3 = plugin.getServer().getPluginManager().getPlugin("Help");
+	if(plugin.permissionsEnabled == false && test != null) { //Permissions check
+	    permissionsFail = false;
+	    if(Pixl.Permissions == null) {
+		String name;
+
+		if(test2 != null) { name = test2.getDescription().getName()+"/FakePermissions "+test.getDescription().getVersion(); } else { name = test.getDescription().getFullName(); }
+		plugin.permissionsType = name;
+		Pixl.Permissions = ((Permissions) test).getHandler();
+		System.out.println("[Pixl] Hooking into Permissions");
+		plugin.permissionsEnabled = true;
+		test = null;
+	    } 
+	} else if(plugin.permissionsEnabled != true && permissionsFail != true) {
+	    permissionsFail = true;
+	    System.out.println("[Pixl] Permissions not found, defaulting to ops.txt");
+	    plugin.permissionsEnabled = false;
 	}
-	
-	@Override
-	public void onPluginEnable(PluginEnableEvent e) {
-		Plugin test = plugin.getServer().getPluginManager().getPlugin("Permissions");
-		Plugin test2 = plugin.getServer().getPluginManager().getPlugin("GroupManager");
-		Plugin test3 = plugin.getServer().getPluginManager().getPlugin("Help");
-		if(plugin.permissionsEnabled == false && test != null) { //Permissions check
-			permissionsFail = false;
-			if(Pixl.Permissions == null) {
-				String name;
-				
-				if(test2 != null) { name = test2.getDescription().getName()+"/FakePermissions "+test.getDescription().getVersion(); } else { name = test.getDescription().getFullName(); }
-				plugin.permissionsType = name;
-				Pixl.Permissions = ((Permissions) test).getHandler();
-				plugin.log.log(Level.INFO, "[Pixl] Hooking into Permissions");
-				plugin.permissionsEnabled = true;
-				test = null;
-			} 
-		} else if(plugin.permissionsEnabled != true && permissionsFail != true) {
-			permissionsFail = true;
-			plugin.log.log(Level.WARNING, "[Pixl] Permissions not found, defaulting to ops.txt");
-			plugin.permissionsEnabled = false;
-		}
-		
-		if(plugin.helpEnabled == false && test3 != null) { //Help check
-			helpFail = false;
-			Help helpPlugin = ((Help) test3);
-			helpPlugin.registerCommand("help Pixl", "Display Pixl's help menu", plugin, true, "pixl.use");
-			helpPlugin.registerCommand("pixl toggle", "Toggle Pixl on/off", plugin, "pixl.use");
-			helpPlugin.registerCommand("pixl set [value]", "Set wool color to byte [value]", plugin, "pixl.use");
-			helpPlugin.registerCommand("pixl clear", "Clear the value set by /pixl set", plugin, "pixl.use");
-			helpPlugin.registerCommand("pixl version", "Detailed version info for Pixl", plugin, "pixl.admin");
-			plugin.log.log(Level.INFO, "[Pixl] Advanced /help support enabled");
-			plugin.helpEnabled = true;
-		} else if(plugin.helpEnabled != true && helpFail != true) {
-			helpFail = true;
-			plugin.log.log(Level.WARNING, "[Pixl] Help plugin isn't detected, Advanced /help support disabled");
-			plugin.helpEnabled = false;
-		}
+
+	if(plugin.helpEnabled == false && test3 != null) { //Help check
+	    helpFail = false;
+	    Help helpPlugin = ((Help) test3);
+	    helpPlugin.registerCommand("help Pixl", "Display Pixl's help menu", plugin, true, "pixl.use");
+	    helpPlugin.registerCommand("pixl toggle", "Toggle Pixl on/off", plugin, "pixl.use");
+	    helpPlugin.registerCommand("pixl set [value]", "Set wool color to byte [value]", plugin, "pixl.use");
+	    helpPlugin.registerCommand("pixl clear", "Clear the value set by /pixl set", plugin, "pixl.use");
+	    helpPlugin.registerCommand("pixl version", "Detailed version info for Pixl", plugin, "pixl.admin");
+	    System.out.println("[Pixl] Advanced /help support enabled");
+	    plugin.helpEnabled = true;
+	} else if(plugin.helpEnabled != true && helpFail != true) {
+	    helpFail = true;
+	    System.out.println("[Pixl] Help plugin isn't detected, Advanced /help support disabled");
+	    plugin.helpEnabled = false;
 	}
-	
-	@Override
-	public void onPluginDisable(PluginDisableEvent e) {
-		Plugin test = plugin.getServer().getPluginManager().getPlugin("Permissions");
-		Plugin test2 = plugin.getServer().getPluginManager().getPlugin("Help");
-		String name = e.getPlugin().getDescription().getName();
-		
-		if((plugin.permissionsEnabled != false && Pixl.Permissions != null && test != null) || (plugin.helpEnabled != false && test2 != null)) {
-			if(name.equals("Permissions")) {
-				plugin.log.log(Level.INFO, "[Pixl] Unhooking Permissions");
-				Pixl.Permissions = null;
-				plugin.permissionsEnabled = false;
-				helpFail = false;
-			} else if(name.equals("Help")) {
-				plugin.log.log(Level.INFO, "[Pixl] Unhooking Help");
-				plugin.helpEnabled = false;
-				helpFail = false;
-			}
-		}
+    }
+
+    @Override
+    public void onPluginDisable(PluginDisableEvent e) {
+	Plugin test = plugin.getServer().getPluginManager().getPlugin("Permissions");
+	Plugin test2 = plugin.getServer().getPluginManager().getPlugin("Help");
+	String name = e.getPlugin().getDescription().getName();
+
+	if((plugin.permissionsEnabled != false && Pixl.Permissions != null && test != null) || (plugin.helpEnabled != false && test2 != null)) {
+	    if(name.equals("Permissions")) {
+		System.out.println("[Pixl] Unhooking Permissions");
+		Pixl.Permissions = null;
+		plugin.permissionsEnabled = false;
+		helpFail = false;
+	    } else if(name.equals("Help")) {
+		System.out.println("[Pixl] Unhooking Help");
+		plugin.helpEnabled = false;
+		helpFail = false;
+	    }
 	}
+    }
 }
